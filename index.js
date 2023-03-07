b3ae238ebe036d31cf987f470421375a88ca0e77
frnkdevelopers <frnkdevelopers@gmail.com>
2023-03-06 22:24:15 -0500

    feat: add controllers, models, routes..

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 //const routerApi = require('./routes');
 const app = express();
+const routes = require("./src/routes");
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({name:'Hello World!'});
 });
 
 // routes
-//routerApi(app);.
+app.use("/v1", routes);
 
 const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
